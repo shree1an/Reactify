e7cca3808e44cc77cc9c4e1fc462adc14074dbe5
shree1an <137431277+shree1an@users.noreply.github.com>
2025-07-29 14:03:59 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
 import './css/App.css'
 
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Home from './pages/Home.jsx';
 import Favorites from './pages/Favorites.jsx';
-
+import LoginRegister from './pages/loginRegister.jsx';
+import { MovieProvider } from "./contexts/MovieContext.jsx";
 import NavBar from './components/NavBar';
 
 
 function App() {
 
-  const movieNumber = 1;
+  const location = useLocation();
+  const hiddenNavRoutes = ['/login-register'];
 
   return (
     <>
-      <div>
-        <NavBar />
-      </div>
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </main>
+      <MovieProvider>
+        <div>
+          {!hiddenNavRoutes.includes(location.pathname) && <NavBar />}
+        </div>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/login-register" element={<LoginRegister />} />
+          </Routes>
+        </main>
+      </MovieProvider>
     </>
 
   )
